3534c6e8874f3be2c1e611d5cc17e25c7ef224d9
wangxindong1999 <15634099908@163.com>
2024-04-12 01:57:57 +0800

    路由懒加载

--- Client/src/router/index.jsx
+++ Client/src/router/index.jsx
@@ -4,24 +4,35 @@ import { Navigate } from "react-router-dom"
 // import Passed from "../view/Passed/Passed"
 // import Pending from "../view/Pending/Pending"
 // import Failed from "../view/Failed/Failed"
-import AllIcon from "../components/Icon/AllIcon"
-import PassedIcon from "../components/Icon/PassedIcon"
-import PendingIcon from "../components/Icon/PendingIcon"
-import FailedIcon from "../components/Icon/FailedIcon"
+const AllIcon = lazy(() => import("../components/Icon/AllIcon"))
+const PassedIcon = lazy(() => import("../components/Icon/PassedIcon"))
+const PendingIcon = lazy(() => import("../components/Icon/PendingIcon"))
+const FailedIcon = lazy(() => import("../components/Icon/FailedIcon"))
+
+// import AllIcon from "../components/Icon/AllIcon"
+// import PassedIcon from "../components/Icon/PassedIcon"
+// import PendingIcon from "../components/Icon/PendingIcon"
+// import FailedIcon from "../components/Icon/FailedIcon"
 // import Details from "../view/Details/Details"
+// import Login from "../view/Login/Login"
+// import Register from "../view/Register/Register"
+// import NotFound from "@/view/404/NotFound"
 
 const basicRoutes = [
   {
     path: "/login",
     component: lazy(() => import("../view/Login/Login")),
+    // component: Login,
   },
   {
     path: "/register",
     component: lazy(() => import("../view/Register/Register")),
+    // component: Register,
   },
   {
     path: "*",
     component: lazy(() => import("../view/404/NotFound")),
+    // component: NotFound,
   },
 ]
 const otherRoutes = [
